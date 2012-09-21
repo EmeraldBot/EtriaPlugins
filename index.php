993624d4de39350fc3bbdc1f613bc36741c8b864
Shawn <MistPhizzle@Gmail.com>
2012-09-20 23:05:48 -0400

    First Commit
    Main styling for the most part done.
    Main page for the most part done.

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
+    <title>EtriaCraft Plugin Suite</title>
+</head>
+<body>
+</body>
+    <div id="mainbox">
+        <div id="smallbox">
+            <h2><u><center>EtriaCraft Plugin Suite</center></u></h2>
+            <p>The following list of plugins were originally designed for the EtriaCraft server, however many of them have been released to the public. To keep an updated list of commands, I have made this little website for them.</p>
+            <p>All of the projects listed below are open source, if you wish to use the source please give the original author (MistPhizzle) credit. Especially if you redistribute.</p>
+            <p>I am open for suggestions / requests for any of the plugins. Use the link to the contact form at the end of the page if you need to reach me. I am also looking for skilled developers who could assist with this plugin.</p>
+        </div>
+        <div id="smallbox">
+            <h3><a href="etriacommands.php">EtriaCommands</a></h3>
+            <dl>
+                <dt>Features:</dt>
+                    <dd>* MySQL / SQLite Support
+                    <dd>* Help Suite. Create custom help pages.
+                    <dd>* Homes Management. Allow users to set named homes, define limits.
+                    <dd>* Backpacks. Set a custom sized backpack for your users, control if they drop on death.
+                    <dd>* Permissions support. Everything has a permission.
+                    <dd>* Admin Suite. Supports bans, kicks, mutes, etc.
+                    <dd>* Warps Management.
+                    <dd>* Weather management.
+                    <dd>* Vanish Suite
+                    <dd>* Much MUCH more.
+        </div>
+        <div id="smallbox">
+            <h3><a href="etriakits.php">EtriaKits</a></h3>
+            <dl>
+                <dt>Features:</dt>
+                <dd>* Define custom kits.
+                <dd>* Custom Cooldown Times.
+                <dd>* Easy permission nodes.
+                <dd>* View the contents of a kit in-game BEFORE receiving the kit.
+                <dd>* Give kits to other players.
+                <dd>* One command, unlimited amount of sub commands.
+        </div>
+        <div id="smallbox">
+            <h3><a href="etriatickets.php">EtriaTickets</a></h3>
+            <p>This plugin is still in the planning stage, no progress has been made. A list of features will be available not too long after initial release.</p>
+        </div>
+        <div id="smallbox">
+            <h3><a href="etriautils.php">EtriaUtils</a></h3>
+            <dl>
+                <dt>Features:</dt>
+                <dd>* Custom Join / Quit Messages.
+                <dd>* Welcomes new players to the server. (Configurable message)
+                <dd>* Permission nodes for saving items / exp.
+                <dd>* Elevators with signs.
+                <dd>* Handles Chat Formatting.
+                <dd>* Chat Suite. New Message Commands: /say, /broadcast, /modchat, /me.
+            </dl>
+        </div>
+        <div id="smallbox">
+            <h3><a href="etriawhitelist.php">EtriaWhitelist</a></h3>
+            <p>This plugin has not yet been made public. It will be available to the public after a bit more testing / feature changes.</p>
+        </div>
+        <div id="smallbox">
+            <h3><a href="contact.php">Contact me</a></h3>
+            <p>Only use the contact form linked above for the following:
+            <ul>
+                <li>Bug Reports / Errors.
+                <li>Questions that aren't answered on these pages / Bukkit.
+                <li>Interested in helping with the development of one of these plugins.
+                <li>Feature Requests / Suggestions.
+    </div>
